d6fa9dec50a8f930c477f91ea7990b0aec660fd5
anonimys2020 <vladislavargun2007@gmail.com>
2025-11-19 15:30:00 +0300

    Add subject management service

---
+++ src/edutrack/application/subjects.py
@@ -0,0 +1,35 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from edutrack.infrastructure.cache.redis import get_cache, set_cache, invalidate
+from edutrack.infrastructure.db import models
+from sqlalchemy import select
+
+SUBJECTS_CACHE_KEY = "cache:subjects"
+SUBJECTS_TTL = 600
+
+
+class SubjectService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_subject(self, name: str) -> models.Subject:
+        subject = models.Subject(name=name)
+        self.session.add(subject)
+        await self.session.commit()
+        await invalidate(SUBJECTS_CACHE_KEY)
+        return subject
+
+    async def list_subjects(self):
+        cached = await get_cache(SUBJECTS_CACHE_KEY)
+        if cached:
+            return cached
+        result = await self.session.scalars(select(models.Subject).order_by(models.Subject.name))
+        subjects = result.all()
+        data = [{"id": str(s.id), "name": s.name} for s in subjects]
+        await set_cache(SUBJECTS_CACHE_KEY, data, ttl_seconds=SUBJECTS_TTL)
+        return data
+
+
+
+
+
